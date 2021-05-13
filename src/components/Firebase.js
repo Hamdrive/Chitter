54a5b470019283f8262a4320162398eeca2c6959
Hamza <hamza.husein017@gmail.com>
2021-05-13 20:05:52 +0530

    feat setup firebase in project

---
+++ src/components/Firebase.js
@@ -0,0 +1,22 @@
+
+
+const firebase = require("firebase/app")
+
+require("firebase/auth")
+require("firebase/firestore")
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAynGPc2xUSKQtXJM3tVG1V4qdGNM5QnGc",
+  authDomain: "chitter-b1dfc.firebaseapp.com",
+  projectId: "chitter-b1dfc",
+  storageBucket: "chitter-b1dfc.appspot.com",
+  messagingSenderId: "521524507667",
+  appId: "1:521524507667:web:73269d103d6758fcd3fbe6",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+export default firebase
