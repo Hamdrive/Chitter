df71cd9e0f569888d29637f3f62cbb68f8904335
Hamza <hamza.husein017@gmail.com>
2021-05-16 19:02:22 +0530

    feat react-router-dom pkg install

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -5,16 +5,14 @@ const firebase = require("firebase/app")
 require("firebase/auth")
 require("firebase/firestore")
 
-const firebaseConfig = {
+firebase.initializeApp( {
   apiKey: "AIzaSyAynGPc2xUSKQtXJM3tVG1V4qdGNM5QnGc",
   authDomain: "chitter-b1dfc.firebaseapp.com",
   projectId: "chitter-b1dfc",
   storageBucket: "chitter-b1dfc.appspot.com",
   messagingSenderId: "521524507667",
   appId: "1:521524507667:web:73269d103d6758fcd3fbe6",
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
