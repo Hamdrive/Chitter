6598358ad41a9781018688f7787ab4e6f4ccb011
Hamza <hamza.husein017@gmail.com>
2021-05-16 20:04:19 +0530

    implement router for index.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { GiSquirrel, GiAcorn } from "react-icons/gi";
 import {} from "react-icons/fa";
 import SidebarComponent from "./SidebarComponent";
@@ -18,25 +19,39 @@ function Sidebar() {
       <div className=" bg-green-500 rounded-full bg-opacity-0 hover:bg-opacity-50 duration-100 cursor-pointer mt-1 py-2 px-2 md:mx-auto lg2:w-16 lg:w-10 md:w-8">
         <GiSquirrel className=" lg2:h-12 lg2:w-12 lg:h-10 lg:w-10 md:h-8 md:w-8" />
       </div>
-
-      <div className="my-5">
-        <SidebarComponent icon={<BiHomeAlt />} text={"Home"} />
-      </div>
-      <div className="my-5">
-        <SidebarComponent icon={<BiHash />} text={"Explore"} />
-      </div>
-      <div className="my-5">
-        <SidebarComponent icon={<BiBell />} text={"Notifications"} />
-      </div>
-      <div className="my-5">
-        <SidebarComponent icon={<BiMessageSquareDetail />} text={"Messages"} />
-      </div>
-      <div className="my-5">
-        <SidebarComponent icon={<BiBookmark />} text={"Bookmarks"} />
-      </div>
-      <div className="my-5">
-        <SidebarComponent icon={<BiUser />} text={"Profile"} />
-      </div>
+      <Link to="/">
+        <div className="my-5">
+          <SidebarComponent icon={<BiHomeAlt />} text={"Home"} />
+        </div>
+      </Link>
+      <Link to="/explore">
+        <div className="my-5">
+          <SidebarComponent icon={<BiHash />} text={"Explore"} />
+        </div>
+      </Link>
+      <Link to="/notifications">
+        <div className="my-5">
+          <SidebarComponent icon={<BiBell />} text={"Notifications"} />
+        </div>
+      </Link>
+      <Link to="/messages">
+        <div className="my-5">
+          <SidebarComponent
+            icon={<BiMessageSquareDetail />}
+            text={"Messages"}
+          />
+        </div>
+      </Link>
+      <Link to="/bookmarks">
+        <div className="my-5">
+          <SidebarComponent icon={<BiBookmark />} text={"Bookmarks"} />
+        </div>
+      </Link>
+      <Link to="/userprofile">
+        <div className="my-5">
+          <SidebarComponent icon={<BiUser />} text={"Profile"} />
+        </div>
+      </Link>
 
       <div className="my-5 mt-6 ">
         <ChitBtn icon={<GiAcorn />} />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
