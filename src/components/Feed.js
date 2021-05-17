bd6c310ea9253fa3db9b2ed0027d471ec48fe226
Hamza <hamza.husein017@gmail.com>
2021-05-17 20:58:32 +0530

    connect firebase to project

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewCheet from "./NewCheet";
 import Cheets from "./Cheets";
+import db from "./Firebase";
 
 function MainFeed() {
+  const [posts, setPosts] = useState([]);
+  const postsUser = db.collection("posts");
+
+  useEffect(() => {
+    getPosts();
+    // eslint-disable-next-line
+  }, []);
+
+  const getPosts = () =>
+    postsUser.onSnapshot((snapshot) => 
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          displayName: doc.data().displayName,
+          userName: doc.data().userName,
+          isVerified: doc.data().isVerified,
+          timestamp: doc.data().timestamp,
+          content: doc.data().content,
+          media: doc.data().media
+        }))
+      )
+    );
+
+  
+
+  console.log(posts)
+
   return (
     <div>
       <div className="h-12 text-2xl font-bold flex items-center pl-8 sticky top-0 border-b-2 border-gray-400 bg-white">
         <h1>Home</h1>
       </div>
       <NewCheet />
-      <Cheets
-        displayName={"Hamza Husein"}
-        isVerified={true}
-        userName={"@itsHamHere"}
-        timestamp={"1hr"}
-        content={"My friends whenever I score more in the test..."}
-        media={"./images/Jen-nerd.gif"}
-      />
-      <Cheets
+      {posts.map((post) => (
+        <Cheets
+          displayName={post.displayName}
+          isVerified={post.isVerified}
+          userName={post.userName}
+          timestamp={
+            post.timestamp
+          }
+          content={post.content}
+          media={post.media}
+        />
+      ))}
+      {/* <Cheets
         displayName={"Hamza Husein"}
         isVerified={true}
         userName={"@itsHamHere"}
@@ -40,7 +72,7 @@ function MainFeed() {
         timestamp={"29min"}
         content={"How long do you think it takes Richmond to apply makeup?"}
         media={"./images/Richmond.gif"}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,20 +1,18 @@
+import firebase from "firebase"
+import "firebase/auth"
+import "firebase/firestore"
 
-
-const firebase = require("firebase/app")
-
-require("firebase/auth")
-require("firebase/firestore")
-
-firebase.initializeApp( {
+const firebaseConfigs = {
   apiKey: "AIzaSyAynGPc2xUSKQtXJM3tVG1V4qdGNM5QnGc",
   authDomain: "chitter-b1dfc.firebaseapp.com",
   projectId: "chitter-b1dfc",
   storageBucket: "chitter-b1dfc.appspot.com",
   messagingSenderId: "521524507667",
   appId: "1:521524507667:web:73269d103d6758fcd3fbe6",
-});
+};
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
+const firebaseApp = firebase.initializeApp(firebaseConfigs)
 
-export default firebase
+const db = firebaseApp.firestore()
+
+export default db;
