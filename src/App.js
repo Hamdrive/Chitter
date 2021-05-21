c483ac0933a2aa73882692a99dc8c2ab418de4f0
Hamza <hamza.husein017@gmail.com>
2021-05-21 20:27:23 +0530

    implement React router in App.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Home from "./views/Home";
+import LandingPage from "./views/LandingPage";
+import UserProfile from "./views/UserProfile";
+import Messages from "./views/Messages";
+import Explore from "./views/Explore";
+import Notifications from "./views/Notifications";
+import Bookmarks from "./views/Bookmarks";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <LandingPage />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/explore">
+              <Explore />
+            </Route>
+            <Route path="/bookmarks">
+              <Bookmarks />
+            </Route>
+            <Route path="/userprofile">
+              <UserProfile />
+            </Route>
+            <Route path="/messages">
+              <Messages />
+            </Route>
+            <Route path="/notifications">
+              <Notifications />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+}
+
+export default App
